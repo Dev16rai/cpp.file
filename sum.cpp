d2e944dbf4c3041ed6cd4eef9d3c95392eed3fe9
Dev Rai <devrai892840@gmail.com>
2026-07-09 18:21:54 +0530

    Initial commit

---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter your number "<<endl;
+    cin>>a>>b;
+    cout<<"this is your result "<<endl<<a+b;
+    return 0;
+}
