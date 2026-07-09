1bd46613e83d4ee5d70f3f0d48dd632dbd790826
Dev Rai <devrai892840@gmail.com>
2026-07-09 18:54:37 +0530

    Added average of two numbers program and gitignore

---
+++ avg-of-two-nums.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter your nums "<<endl;
+    cin>>a>>b;
+
+    float result = (a+b)/2.0;
+
+    cout<<"this is your avg of this Two Nums "<<endl<<result;
+    return 0;
+}
