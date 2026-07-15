70f5493fdd3bebf2d7e857ad6cea83f79b5455c3
Dev Rai <devrai892840@gmail.com>
2026-07-15 23:57:46 +0530

    basic operators

---
+++ A-basic-Folder/Arithmetic-operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y,p,q,r,s,t;
+    cout<<"enter "<<endl;
+    cin >> x >> y;
+
+     p=x+y;
+     q=x-y;
+     r=x*y;
+     s=x/y;
+     t=x%y;
+    
+    cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+    return 0;
+}  
