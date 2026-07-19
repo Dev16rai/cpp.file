114599eb1a6e7661f34a5136012674887e33ae4b
Dev Rai <devrai892840@gmail.com>
2026-07-19 10:20:58 +0530

    added basic

---
+++ A-basic-Folder/gfgQ-print-last-digit.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter your nums "<<endl;
+    cin >> n;
+
+   int c = abs(n%10);
+   cout<<c;   
+    return 0; 
+}
