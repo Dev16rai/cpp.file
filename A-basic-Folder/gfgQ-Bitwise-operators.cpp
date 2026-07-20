2fb4503458e88644567e025d3b1e30468653dc18
Dev Rai <devrai892840@gmail.com>
2026-07-20 15:15:08 +0530

    added basic gfgQ

---
+++ A-basic-Folder/gfgQ-Bitwise-operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    // code here
+    
+    int d = a ^ a;
+    int e = c ^ b;
+    int f = a & b;
+    int g = ~e;
+
+    cout << d << " " << e << " " << f << " " << g << endl;
+    return 0;
+}
---
+++ A-basic-Folder/gfgQ-typecast.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cout<<"enter your number "<<endl;
+    cin >> d;
+
+    // code here
+    int b = (int)d;
+    cout<<b;
+    return 0;
+}
