ed36094d9ab1ba53193fbfbfb88e755b6a380432
Dev Rai <devrai892840@gmail.com>
2026-07-20 15:16:04 +0530

    added if statement gfgQ

---
+++ B-code/B-if-else-Folder/gfgQ-if-statement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    
+    if(n<=100){
+        cout<<"Number";
+    }
+    else{
+        cout<<"Big"<<endl<<"Number";
+    }
+    return 0;
+}
