75686e817bdcfb1e593f11f88de9b8fe5e4d898a
Dev Rai <devrai892840@gmail.com>
2026-07-23 22:36:04 +0530

    added basic string question

---
+++ A-basic-Folder/basic-of-string-user-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string a, b, separator;
+    cout<<"enter your word "<<endl;
+    cin >> a >> b >> separator;
+
+    // code here
+    cout<<a<<separator<<b;
+
+    return 0;
+}
