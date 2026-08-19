3170724bd71f31a4299e09cf61df84b7586d5f62
Dev Rai <devrai892840@gmail.com>
2026-08-19 20:31:42 +0530

    add Q of atring to check it is empty or not

---
+++ E.String-Folder/Check-f-string-is-empty.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+     string s;
+     cout<<"enter your srting "<<endl;
+     getline(cin,s);
+
+
+        if(s.empty()){
+          cout<<"it is empty";
+        }
+        else{
+            cout<<"its have a some char ";
+        }
+    
+    return 0;
+}
+
