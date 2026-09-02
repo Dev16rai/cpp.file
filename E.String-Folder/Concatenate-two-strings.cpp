f66e8162e3fcb33359a76c701d5499033639d345
Dev Rai <devrai892840@gmail.com>
2026-09-02 21:35:30 +0530

    Add Question of string plus two string

---
+++ E.String-Folder/Concatenate-two-strings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+string a;
+cout<<"enter your first string "<<endl;
+cin>>a;
+
+string b;
+cout<<"enter your second string "<<endl;
+cin>>b;
+
+cout<<a+""+b;
+}
